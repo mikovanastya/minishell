b3e3529bd7c53aebf371adb793ff03ec8cf0a1dd
Ella Ward <eward@at-l6.msk.21-school.ru>
2022-06-09 19:27:09 +0300

    blin ne poluchilos

--- parser/substitute_envp_1.c
+++ parser/substitute_envp_1.c
@@ -60,10 +60,10 @@ int	repl_less(char **to_change, char *repl, int i, int a)
 	int		j;
 
 	b = ft_strlen(repl);
-	t = ft_strdup(*to_change);
-	// free(*to_change);
-	// *to_change = (char *)malloc(sizeof(char *) * (ft_strlen(t) + b - a + 1));
-	ft_strlcpy(*to_change, t, ft_strlen(t));
+	// t = ft_strdup(*to_change);
+	// // free(*to_change);
+	// // *to_change = (char *)malloc(sizeof(char *) * (ft_strlen(t) + b - a + 1));
+	// ft_strlcpy(*to_change, t, ft_strlen(t));
 	ft_memset(*to_change + ft_strlen(t), 'g', ft_strlen(t) + b - a + 1);
 	j = ft_strlen(t) + b - a;
 	while (j - (b - a) > i)
@@ -78,47 +78,6 @@ int	repl_less(char **to_change, char *repl, int i, int a)
 	return (0);
 }
 
-// 	char	*t;
-// 	int		j;
-// 	int		b;
-// 	char	*h;
-
-// 	h = *to_change;
-// 	printf("repl less\n");
-// 	b = ft_strlen(repl);
-// 	t = ft_strdup(h);
-// 	ft_bzero(h, ft_strlen(h));
-// 	free(h);
-// 	h = (char *)malloc(sizeof(char *) * (ft_strlen(t) + b - a + 1));
-// 	if (!h)
-// 	{
-// 		free(h);
-// 		return(0);
-// 	}
-// 	ft_bzero(h, ft_strlen(t) + b - a - 1);
-// 	ft_strlcpy(h, t, ft_strlen(t) + 1);
-// 	j = ft_strlen(t) + b - a;
-// 	//printf("\n j is %d | b is %d | a is %d | ft_strlen(t) is %zu\n", j, b, a, ft_strlen(t));
-// 	free(t);
-// 	*(h + j) = '\0';
-// 	j--;
-// 	while(j - (b - a) > i + a - 1)
-// 	{
-// 		//printf("%c ", *(*to_change + j - (b - a)));
-// 		*(h + j) = *(*to_change + j - (b - a));
-// 		j--;
-// 	}
-// 	*(h + i + j) = '\0';
-// 	//repl_equal(&h, repl, i);
-// 	j = 0;
-// 	while (*(repl + j))
-// 	{
-// 		*(h + j + i) = *(repl + j);
-// 		j++;
-// 	}
-// 	*to_change = h;
-// 	return(0);
-// }
 /*возвращает на сколько надо двинуться вперед*/
 int	replace(char **to_change, int i)
 {
