90a794ad1cdca5911513656c7684347f2dc1b244
Ella Ward <eward@at-e2.msk.21-school.ru>
2022-06-18 13:49:31 +0300

    almost norm

--- parser/substitute_envp_1.c
+++ parser/substitute_envp_1.c
@@ -22,7 +22,7 @@ int	repl_equal(char **to_change, char *repl, int i)
 		*(*to_change + i + j) = *(repl + j);
 		j++;
 	}
-	return (0);
+	return (i + ft_strlen(repl));
 }
 
 int	repl_more(char **to_change, char *repl, int i, int a)
@@ -30,7 +30,6 @@ int	repl_more(char **to_change, char *repl, int i, int a)
 	int		j;
 	int		b;
 
-	printf("repl more\n");
 	b = ft_strlen(repl);
 	j = 0;
 	while (*(*to_change + i + j + a - b))
@@ -40,7 +39,7 @@ int	repl_more(char **to_change, char *repl, int i, int a)
 	}
 	*(*to_change + i + j) = '\0';
 	repl_equal(to_change, repl, i);
-	return (0);
+	return (i + ft_strlen(repl));
 }
 
 int	delete_var(char **to_change, int i, int a)
@@ -51,7 +50,7 @@ int	delete_var(char **to_change, int i, int a)
 		i++;
 	}
 	*(*to_change + i) = '\0';
-	return (0);
+	return (i + 1);
 }
 
 int	repl_less(char **to_change, char *repl, int i, int a)
@@ -62,18 +61,16 @@ int	repl_less(char **to_change, char *repl, int i, int a)
 
 	b = ft_strlen(repl);
 	t = ft_strdup(*to_change);
-	printf("\n t %s\n", t);
 	j = ft_strlen(t);
 	while (j > i + a - 1)
 	{
-		printf("\n to change %c\n", (*to_change)[j]);
 		(*to_change)[j + b - a] = (*to_change)[j];
 		j--;
 	}
 	repl_equal(to_change, repl, i);
 	*(*to_change + ft_strlen(t) + b - a) = '\0';
 	free(t);
-	return (0);
+	return (i + ft_strlen(repl));
 }
 
 /*возвращает на сколько надо двинуться вперед*/
@@ -88,36 +85,21 @@ int	replace(char **to_change, int i)
 	a = 1;
 	var_name = NULL;
 	if (*(*to_change + i + 1) == '$')
-	{
-		repl_less(to_change, ft_itoa(g_shell.pid), i, 2);
-		return (i + ft_strlen(ft_itoa(g_shell.pid)));
-	}
-	while (ft_isalpha(*(*to_change + i + a)) || *(*to_change + i + a) == '_' || (ft_isdigit(*(*to_change + i + a)) && *(*to_change + i + a) != 1))
+		return (repl_less(to_change, ft_itoa(g_shell.pid), i, 2));
+	while (ft_isalpha(*(*to_change + i + a)) || *(*to_change + i + a) == '_'
+		|| (ft_isdigit(*(*to_change + i + a)) && *(*to_change + i + a) != 1))
 		a++;
 	var_name = (char *)malloc(sizeof(char) * (a + 1));
 	ft_memmove(var_name, *to_change + i + 1, a - 1);
 	repl = find_var(var_name);
 	if (repl == 0)
-	{
-		delete_var(to_change, i, a);
-		rez = i + 1;
-	}
+		rez = delete_var(to_change, i, a);
 	else if (a == (int)ft_strlen(repl))
-	{
-		repl_equal(to_change, repl, i);
-		rez = i + ft_strlen(repl);
-	}
+		rez = repl_equal(to_change, repl, i);
 	else if (a > (int)ft_strlen(repl))
-	{
-		repl_more(to_change, repl, i, a);
-		rez = i + ft_strlen(repl);
-	}
+		rez = repl_more(to_change, repl, i, a);
 	else if (a < (int)ft_strlen(repl))
-	{
-		repl_less(to_change, repl, i, a);
-		rez = i + ft_strlen(repl);
-	}
-	printf("repl %s\n", repl);
+		rez = repl_less(to_change, repl, i, a);
 	if (repl)
 		free(repl);
 	if (var_name)
