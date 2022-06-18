a996bd16052d48cf3e54f4cbddcfa9522bf14cf3
Ella Ward <eward@at-e2.msk.21-school.ru>
2022-06-18 13:01:53 +0300

    works with another symbols in str

--- parser/substitute_envp_1.c
+++ parser/substitute_envp_1.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
 int	repl_equal(char **to_change, char *repl, int i)
 {
 	int	j;
@@ -18,10 +19,10 @@ int	repl_equal(char **to_change, char *repl, int i)
 	j = 0;
 	while (*(repl + j))
 	{
-		*(*to_change + i + j) = *(repl  + j);
+		*(*to_change + i + j) = *(repl + j);
 		j++;
 	}
-	return(0);
+	return (0);
 }
 
 int	repl_more(char **to_change, char *repl, int i, int a)
@@ -32,14 +33,14 @@ int	repl_more(char **to_change, char *repl, int i, int a)
 	printf("repl more\n");
 	b = ft_strlen(repl);
 	j = 0;
-	while(*(*to_change + i + j + a - b))
+	while (*(*to_change + i + j + a - b))
 	{
 		*(*to_change + i + j) = *(*to_change + i + j + a - b);
 		j++;
 	}
 	*(*to_change + i + j) = '\0';
 	repl_equal(to_change, repl, i);
-	return(0);
+	return (0);
 }
 
 int	delete_var(char **to_change, int i, int a)
@@ -61,10 +62,12 @@ int	repl_less(char **to_change, char *repl, int i, int a)
 
 	b = ft_strlen(repl);
 	t = ft_strdup(*to_change);
-	j = ft_strlen(t) + b - a;
-	while (j - (b - a) > i)
+	printf("\n t %s\n", t);
+	j = ft_strlen(t);
+	while (j > i + a - 1)
 	{
-		(*to_change)[j - b] = (*to_change)[j];
+		printf("\n to change %c\n", (*to_change)[j]);
+		(*to_change)[j + b - a] = (*to_change)[j];
 		j--;
 	}
 	repl_equal(to_change, repl, i);
@@ -82,12 +85,11 @@ int	replace(char **to_change, int i)
 	int		rez;
 
 	rez = 0;
-	a = 0;
-	a++;
+	a = 1;
 	var_name = NULL;
 	if (*(*to_change + i + 1) == '$')
 	{
-		repl_less(to_change, ft_itoa(g_shell.pid), i, a);
+		repl_less(to_change, ft_itoa(g_shell.pid), i, 2);
 		return (i + ft_strlen(ft_itoa(g_shell.pid)));
 	}
 	while (ft_isalpha(*(*to_change + i + a)) || *(*to_change + i + a) == '_' || (ft_isdigit(*(*to_change + i + a)) && *(*to_change + i + a) != 1))
@@ -97,7 +99,7 @@ int	replace(char **to_change, int i)
 	repl = find_var(var_name);
 	if (repl == 0)
 	{
-		delete_var(to_change, i,  a);
+		delete_var(to_change, i, a);
 		rez = i + 1;
 	}
 	else if (a == (int)ft_strlen(repl))
@@ -107,7 +109,7 @@ int	replace(char **to_change, int i)
 	}
 	else if (a > (int)ft_strlen(repl))
 	{
-	 	repl_more(to_change, repl, i, a);
+		repl_more(to_change, repl, i, a);
 		rez = i + ft_strlen(repl);
 	}
 	else if (a < (int)ft_strlen(repl))
